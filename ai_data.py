f7e49c9c4b772c5db564bdb1bde144b709aa3a95
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-08-24 02:45:30 -0400

    progressing

--- ai_data.py
+++ ai_data.py
@@ -1,21 +1,27 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import asyncio
 import json
 import re
-from collections import OrderedDict, defaultdict
+from collections import (OrderedDict, defaultdict)
 from configparser import ConfigParser
 from dataclasses import dataclass
-from functools import lru_cache, wraps
+from functools import (lru_cache, wraps)
 from pathlib import Path
 from pprint import pprint
-from typing import NamedTuple, Union, Literal, Type
-# import rasa
-from bs4 import BeautifulSoup
-from aiohttp import ClientSession, TCPConnector, client_exceptions
-from ascii_graph import Pyasciigraph
-from nested_lookup import nested_lookup as nested
-from rapidfuzz import fuzz, process
-from unidecode import unidecode
 from random import choice
+from typing import (Literal, NamedTuple, Type, Union)
+
+# import rasa
+# import tensorflow as tf
+from aiohttp import (ClientSession, TCPConnector, client_exceptions)
+from ascii_graph import Pyasciigraph
+from bs4 import BeautifulSoup
+from nested_lookup import nested_lookup as nested
+from rapidfuzz import (fuzz, process)
+from transformers import (MarianConfig, MarianMTModel, MarianTokenizer, pipeline)
+from unidecode import unidecode
 
 
 class ConfigInfo:
@@ -44,19 +50,47 @@ class SingletonMeta(type):
             cls._instances[cls] = instance
         return cls._instances[cls]
 
+class Translate:
+    text = None
+    
+    @classmethod
+    @lru_cache(maxsize=1)
+    def _translate_text(cls, text, src_lang, tgt_lang):
+        model_name = f'Helsinki-NLP/opus-mt-{src_lang}-{tgt_lang}'
+        config = MarianConfig.from_pretrained(model_name, revision="main")
+        model = MarianMTModel.from_pretrained(model_name, config=config)
+        tokenizer = MarianTokenizer.from_pretrained(model_name, config=config)
+        translation = pipeline("translation", model=model, tokenizer=tokenizer)
+        translated_text = translation(text, max_length=4096, return_text=True)[0]
+        cls.text = translated_text.get('translation_text')
+        return translated_text.get('translation_text')
+
+    @property
+    def translate(self, *args):
+        return self._translate_text(*args)
+
+    @staticmethod
+    def best_match(string, **kwargs):
+        #?> Add a check ratio method
+        values_ = kwargs.get('values_', ['test1', 'test2', 'test3'])
+        if values_ and not isinstance(values_, (dict, OrderedDict, defaultdict)):
+            values_ = {value: key for value, key in enumerate(values_)}
+        match_ = process.extractOne(string.lower(), [i.lower() for i in values_.values()], scorer=fuzz.ratio)
+        matched = match_[0].upper() if all(i.isupper() for i in values_.values()) else match_[0].title()
+        return matched, match_
+
 @dataclass
-class BaseAPI:
+class BaseAPI(Translate):
     config: None=ConfigInfo()
     path: None=Path(__file__).parent.absolute() / 'islamic_data'
 
 class QuranAPI(BaseAPI, metaclass=SingletonMeta):
-    def __init__(self, _holder: Type=None):
-        self.url = BaseAPI.config.quran_url
+    def __init__(self):
+        self.url = self.config.quran_url
         self.headers = {
-            "X-RapidAPI-Key": BaseAPI.config.quran_api,
-            "X-RapidAPI-Host": BaseAPI.config.quran_host
+            "X-RapidAPI-Key": self.config.quran_api,
+            "X-RapidAPI-Host": self.config.quran_host
         }
-        self._holder = _holder
     
     async def _request(self, endpoint: Union[int, str], **kwargs):
         default_values = ['']*4 + [{}]
@@ -111,7 +145,7 @@ class QuranAPI(BaseAPI, metaclass=SingletonMeta):
                 new_stats = self._format_stats(stats, type_=dict, format_=1)
                 return new_stats
         except (AttributeError) as e:
-            print('Modify \'Pyasciigraph\' module!\n Change all \'collections.Iterable\' -> collections.abc.Iterable')
+            print('Modify \'Pyasciigraph\' module!\n Change all \'collections.abc.Iterable\' -> collections.abc.Iterable')
 
     async def _extract_surahs(self, export=False):
         surahs = {}
@@ -133,22 +167,12 @@ class QuranAPI(BaseAPI, metaclass=SingletonMeta):
         surahs = OrderedDict(sort_json)
         return surahs
     
-    def best_match(self, string, **kwargs):
-        #?> Add a check ratio method
-        surah_names = self._list_surahs
-        values_ = kwargs.get('values_', surah_names)
-        if values_ and not isinstance(values_, (dict, OrderedDict, defaultdict)):
-            values_ = {value: key for value, key in enumerate(values_)}
-        match_ = process.extractOne(string.lower(), [i.lower() for i in values_.values()], scorer=fuzz.ratio)
-        matched = match_[0].upper() if all(i.isupper() for i in values_.values()) else match_[0].title()
-        return matched
-    
-    async def minimize_surah(self, **kwargs):
-        default_values = (1, 'fatiha', 'en')
-        surah_id, name, lang = (kwargs.get(key, default_values[i]) for i, key in enumerate(('surah_id', 'name', 'lang')))
-        if name and not surah_id:
-            name = self.best_match(name)
-            ...
+    # async def minimize_surah(self, **kwargs):
+    #     default_values = (1, 'fatiha', 'en')
+    #     surah_id, name, lang = (kwargs.get(key, default_values[i]) for i, key in enumerate(('surah_id', 'name', 'lang')))
+    #     if name and not surah_id:
+    #         name = self.best_match(name)
+    #         ...
     
     @lru_cache(maxsize=1)
     @staticmethod
@@ -156,23 +180,22 @@ class QuranAPI(BaseAPI, metaclass=SingletonMeta):
         #!> Modify for flexibility
         return json.load(open(path / f'{name}.{type_}', mode=mode, encoding=encoding))
 
-class HadithAPI(BaseAPI, metaclass=SingletonMeta):
-    def __init__(self, _holder: Type=None):
-        super().__init__(_holder)
-        self.url = BaseAPI.config.hadith_url
+class HadithAPI(QuranAPI):
+    def __init__(self):
+        super().__init__()
+        self.url = self.config.hadith_url
         self.headers = None
-        self._holder = _holder
 
     async def _extract_urls(self, **kwargs):
         async def _parser(contents):
             for book, link in contents.items():
                 with open(self.path / f'book_{book}.json', mode='w', encoding='utf-8') as file2:
-                    hadith_json = await self._holder._request('', slash=False, url=link)
+                    hadith_json = await self._request('', slash=False, url=link)
                     json.dump(hadith_json, file2, indent=4)
             return file2
         default_values = (False, Literal[True], 'English')
         parser, _, lang = (kwargs.get(key, default_values[i]) for i,key in enumerate(('parser', 'export', 'lang')))
-        json_file = await self._holder._request('', slash=False, url=self.url)
+        json_file = await self._request('', slash=False, url=self.url)
         contents_ = [(nested('book', j, wild=True), nested('link', j, wild=True)) for i in json_file.values() for j in i['collection'] if j.get('language') == lang]
         contents = {key[0][0]: key[1][0] for key in contents_}
         
@@ -181,7 +204,7 @@ class HadithAPI(BaseAPI, metaclass=SingletonMeta):
             json.dump(contents, file, indent=4)
             return contents
         if parser:
-            json_file = self._holder._load_file(path=self.path, name='hadith_api_links', mode='r')
+            json_file = self._load_file(path=self.path, name='hadith_api_links', mode='r')
             await _parser(json_file)
             return contents
         else:
@@ -196,7 +219,7 @@ class HadithAPI(BaseAPI, metaclass=SingletonMeta):
         default_values = ['', False]
         author, _  = [kwargs.get(key, default_values[i]) for i, key in enumerate(('author', ''))]
         if author:
-            author = self._holder.best_match(author, values_=book_authors)
+            author = self.best_match(author, values_=book_authors)[0]
             book_json = json.loads((self.path / f'book_{author}.json').read_text(encoding='utf-8'))
             return book_json
         else:
@@ -206,19 +229,18 @@ class HadithAPI(BaseAPI, metaclass=SingletonMeta):
         ...
 
 
-class IslamFacts(BaseAPI, metaclass=SingletonMeta):
+class IslamFacts(QuranAPI):
     facts = set()
     allah_names = dict()
     
-    def __init__(self, _holder=None):
-        super().__init__(_holder)
+    def __init__(self):
+        super().__init__()
         self.url = BaseAPI.config
         self.headers = None
-        self._holder = _holder
     
     @classmethod
     def _update_facts(cls, facts: set):
-        file2 = cls._holder._load_file(path=cls.path, name='islam_facts', mode='r')
+        file2 = cls._load_file(path=cls.path, name='islam_facts', mode='r')
         fun_facts = dict.fromkeys(file2)
         fun_facts.update(facts)
         cls.facts.update(facts)
@@ -239,7 +261,7 @@ class IslamFacts(BaseAPI, metaclass=SingletonMeta):
             #!> FunFact generator website only allows ~18 free SAME random facts
             while len(self.facts) <= 18:
                 for _ in range(limit):
-                    html_content = await self._holder._request('', slash=False, url=self.url.islam_facts)
+                    html_content = await self._request('', slash=False, url=self.url.islam_facts)
                     soup = BeautifulSoup(html_content, 'html.parser')
                     fun_fact = soup.find_all('h2')[0].text
                     formatted = re.sub(r'\((Religion > Islam )\)', '', fun_fact).strip()
@@ -258,7 +280,7 @@ class IslamFacts(BaseAPI, metaclass=SingletonMeta):
     async def _extract_contents(self, **kwargs):
         default_values = ['']*3+[False, self.url.myislam]
         endpoint, class_, tag_, slash, url = tuple(kwargs.get(key, default_values[i]) for i,key in enumerate(('endpoint', 'class_', 'tag_', 'slash', 'url',)))
-        main_page = await self._holder._request(endpoint=endpoint, slash=slash, url=url)
+        main_page = await self._request(endpoint=endpoint, slash=slash, url=url)
         soup = BeautifulSoup(main_page, 'html.parser')
         params = {}
         if (class_) and not (tag_):
@@ -284,15 +306,21 @@ class IslamFacts(BaseAPI, metaclass=SingletonMeta):
             return await self._extract_contents(**kwargs)
         
         def extract_name_data(html_contents):
+            #? Arabic encoding [\u0600-\u06FF]
+            summary = sorted(''.join([i.text for i in html_contents[3]]).split('\n'), key=len, reverse=True)[0]
+            # transliteration_ar = [i for i in summary.split() if 75<=self.best_match(i[3:], values_={key:value[3:] for key,value in names.items()})[1][1]<=100]
+            # t = [[i[0], i[1]] for i in transliteration_ar]
+            # print(summary)
+            # print(transliteration_ar)
             contents = {
-                'transliteration': re.sub(r'[()]', '', html_contents[0][0].text),
+                'transliteration_eng': re.sub(r'[()]', '', html_contents[0][0].text),
+                'transliteration_ar': '',
                 'description': [i.text for i in html_contents[1]],
                 'mentions-from-quran-hadith': [i.text for i in html_contents[2]],
-                'summary': sorted(''.join([i.text for i in html_contents[3]]).split('\n'), key=len, reverse=True)[0]
+                'summary': summary
             }
             modified_contents = {key: ''.join(value) for key,value in contents.items()}
             return modified_contents
-        
         main_page = await extract_content(endpoint='', slash=False, url=self.url.myislam, class_='transliteration')
         names = await self._allah_99names(main_page)
         all_name_contents = {}
@@ -311,7 +339,7 @@ class IslamFacts(BaseAPI, metaclass=SingletonMeta):
     @classmethod
     @property
     def allah_99names(cls):
-        #!> Add Exception Handling
+        #!> Add Exception Handling if None
         return cls.allah_names
     
     @classmethod
@@ -340,10 +368,11 @@ class IslamFacts(BaseAPI, metaclass=SingletonMeta):
 
 async def main():
     a = QuranAPI()
-    b = HadithAPI(a)
-    c = IslamFacts(a)
+    b = HadithAPI()
+    c = IslamFacts()
     d = await c._get_name_contents()
-    pprint(d)
+    # print(d)
+    # pprint(nested('transliteration_ar', d, wild=True))
     
 if __name__ == '__main__':
     asyncio.run(main())
