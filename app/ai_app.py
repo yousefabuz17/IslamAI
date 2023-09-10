332b9ffcc730def7519fa5f10c24109462bc1f4f
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-09-09 21:41:12 -0400

    Testing GitLab Pipeline

---
+++ app/ai_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Test Run'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "API Endpoint and GitLab working successfully"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
