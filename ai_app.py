544b82294907fe503f4f7699a17ab924dc1e5b13
Yousef Abuzahrieh <yousef.abuzahrieh@gmail.com>
2023-09-10 21:24:06 -0400

    .

--- ai_app.py
+++ ai_app.py
@@ -13,4 +13,4 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
