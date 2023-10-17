4bb93dee0a0a8175e599040020fa51ef5eb6d21a
Yousef Abuzahrieh <yousef.abuzahrieh@gmail.com>
2023-10-16 22:46:37 -0400

    .

--- ai_model.py
+++ ai_model.py
@@ -1,27 +1,100 @@
-# import os
+import re
+from os import environ
+import logging
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+environ["KMP_AFFINITY"] = "noverbose"
+from collections import OrderedDict
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization # type: ignore
+from nltk.sentiment import SentimentAnalyzer, SentimentIntensityAnalyzer
+from nltk.sentiment.util import extract_unigram_feats, mark_negation
+from nested_lookup import nested_lookup as nested
+from blueprints.data_loader import (stopwords, loader, ArgMapper, DataLoader as dl)
 
-# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-# import rasa
-# import tensorflow as tf
-# from transformers import MarianConfig, MarianMTModel, MarianTokenizer, pipeline
-# import gensim
-# from gensim.models import Word2Vec
-# from nltk.tokenize import word_tokenize
 
-class Translate:
+load, translate = (getattr(dl, i) for _,i in enumerate(('load_file', 'translate')))
+folder_names = ('jsons', 'hadiths', 'pillars', 'salah')
+data = ArgMapper({folder: loader(key=folder) for _,folder in enumerate(folder_names)})
+
+sentiment_analyzer, intensity_analyzer = SentimentAnalyzer(), SentimentIntensityAnalyzer()
+feature_extractors = [extract_unigram_feats, mark_negation]
+
+class TextProcessor:
+    def __init__(self, text: str, **kwargs):
+        self.kwargs = kwargs
+        self.text = self._filter_stopwords(text)
+        self._vectorizer = self._get_vector()
+        self.vectorized_sequences = self._get_vect_sequences()
+        self.polarity = self._get_polarity(text)
+
+    def _get_vector(self):
+        default_values = (100, None, 'int', 100, None)
+        default_args = ('max_tokens', 'ngrams', 'output_mode', 'output_sequence_length')
+        new_kwargs = {arg: self.kwargs.get(arg, value) for arg, value in zip(default_args, default_values)}
+        return TextVectorization(**new_kwargs)
     
-    # @classmethod
-    # @lru_cache(maxsize=1)
-    # def _translate_text(cls, *args):
-    #     text, src_lang, tgt_lang = args
-    #     model_name = f'Helsinki-NLP/opus-mt-{src_lang}-{tgt_lang}'
-    #     config = MarianConfig.from_pretrained(model_name, revision="main")
-    #     model = MarianMTModel.from_pretrained(model_name, config=config)
-    #     tokenizer = MarianTokenizer.from_pretrained(model_name, config=config)
-    #     translation = pipeline("translation", model=model, tokenizer=tokenizer)
-    #     translated_text = translation(text, max_length=512, return_text=True)[0]
-    #     return translated_text.get('translation_text')
+    @staticmethod
+    def _filter_stopwords(text):
+        text = text.split('-') if re.match(r'^\S*$', text) else text.split()
+        stopwords_contents = [' '.join(getattr(stopwords, i)) for i in vars(stopwords).keys() if re.match(r'^[a-z].*[a-z]$', i)]
+        flattened_stopwords = ' '.join([i.lower().strip() for i in stopwords_contents]).split()
+        # compiler = re.compile('|'.join([re.escape(i) for i in flattened_stopwords]))
+        return [i for i in text if i.lower() not in flattened_stopwords]
 
-    # def translate(self, *args):
-    #     return self._translate_text(*args)
-    pass
+    def _get_vect_sequences(self):
+        self._vectorizer.adapt(self.text)
+        return self._vectorizer(self.text)
+    
+    def _get_polarity(self, text):
+        return intensity_analyzer.polarity_scores(text)
+    
+    def get_config(self):
+        return self._vectorizer.get_config()
+
+    def get_vocabulary(self):
+        vocabs = self._vectorizer.get_vocabulary()
+        vocabs[0] = self.kwargs.get('oov_token', '[START]')
+        return vocabs
+
+    @property
+    def word_index(self):
+        return {word: index for word, index in zip(self.get_vocabulary(), range(0, len(self.get_vocabulary()) + 1))}
+
+def process_text(text, **kwargs):
+    return TextProcessor(text, **kwargs)
+
+class QuranModel:
+    '''
+    ~ TextProcessing and Training for the Quran in all available languages.
+    ~ Files: Altafsir & SurahQuran
+    '''
+    _altafsir = data.jsons['all-surahs-altafsir']
+    _surahquran = data.jsons['all-surahs-surahquran']
+    
+    def __init__(self):
+        self.altafsir = self._process_altafsir(self._altafsir)
+        self.surahquran = self._process_surahquran(self._surahquran)
+    
+    def _process_altafsir(self, file):
+        ...
+    
+    def _process_surahquran(self, file):
+        all_surah_names = list(file.keys())
+        all_lang_verses = OrderedDict({all_surah_names[idx]: {**lang_verses} for idx, lang_verses in enumerate(nested('verses', file))})
+        all_contents = []
+        return all_lang_verses['1-Al-Fatihah']['English']
+
+test1 = load(path='jsons/quran/altafsir', file_name='1-Al-Fatihah')['text']
+test2 = QuranModel().surahquran
+lst = list(test2.values())[0]
+# print(test2)
+tf_tokenizer = process_text(lst, max_tokens=100)
+#     a[idx].append(ArgMapper({key: getattr(tf_tokenizer, i) for _,i in enumerate(('vectorized_sequences', 'word_index', 'polarity', 'text'))}))
+print(tf_tokenizer.vectorized_sequences)
+print(tf_tokenizer.word_index)
+print(tf_tokenizer.polarity)
+print(tf_tokenizer.text)
+
+# print([getattr(tf_tokenizer, i) for _,i in enumerate(('vectorized_sequences', 'word_index', 'polarity'))], sep='\n\n\n')
+
--- blueprints/data_loader.py
+++ blueprints/data_loader.py
@@ -1,34 +1,100 @@
+import re
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from multiprocessing import cpu_count
 from pathlib import Path
 from collections import OrderedDict
 from functools import lru_cache
+from dataclasses import dataclass
 
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+from typing import Union
 
-class DataLoader:
-    def __init__(self, folder_path='jsons', file_ext='json'):
+@dataclass
+class ArgMapper:
+    _p_string = '\n[ATTRIBUTES FOR {}]\n{}'
+    
+    def __init__(self, dict_, dont_map=False, _p_key=None, paths=None):
+        self._p_key = f'{_p_key} ({self.__class__.__name__} instance)'
+        self._data_files = ((key, value) for key,value in dict_.items())
+        if dont_map and paths:
+            for path in paths:
+                file_name = path.stem
+                self._data_files[file_name] = path
+        for key, value in dict_.items():
+            setattr(self, key, value)
+    
+    def __str__(self):
+        return self.__repr__()
+    
+    def __repr__(self):
+        str_ = '''
+        1. To Obtain contents of files, use the name of the file as the property name.
+        2. If no file names are shown, use `get_files` property for all files (PosixPaths)
+        '''
+        lst = [i for i in dir(self) if not re.match(r'^\_[a-z]', i)]
+        return f'{str_}\n{self._p_string.format(self._p_key, lst)}'
+    
+    @property
+    def get_files(self):
+        return self._data_files
+    
+    def get(self, key, default=None):
+        try:
+            return getattr(self, key)
+        except AttributeError:
+            return default
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+class DataLoader(ArgMapper):
+    def __init__(self, folder_path='jsons', file_ext='json', ext_path=''):
+        '''
+        1. Use self.get_files if folder contains directories
+        2. Use folder_path for islamic_data purposes (e.g 'jsons/quran/altafsir')
+        '''
+        self.ext_path = ext_path
         self.file_ext = file_ext
-        self.path = Path(__file__).parent.parent.absolute() / 'islamic_data' / folder_path
-        self.data_files = self.path.rglob(f'*.{file_ext}')
+        self.path = Path(__file__).parent.parent.absolute() / 'islamic_data' / folder_path if not self.ext_path else Path(self.ext_path)
+        self._data_files = self._get_all_files()
 
+    def _get_all_files(self):
+        return self.path.rglob(f'*.{self.file_ext}' if not all([self.ext_path, self.file_ext]) else '*')
+    
+    @property
+    def get_files(self):
+        return self._data_files
+    
+    def __str__(self):
+        if isinstance(self, DataLoader):
+            return self.__repr__()
+        elif isinstance(self, ArgMapper):
+            return super().__str__()
+    
+    def __repr__(self):
+        str_ = '''Options to obtain file contents:
+                    1. Turn DataLoader to ArgMapper -> 
+                        i. pass argument `mapper` when calling loader (e.g DataLoader(dict_, **kwargs)(mapper=True))
+                        ii. use .mapper property for conversion (e.g DataLoader(dict_, **kwargs).mapper)
+                    2. Use `get_files` property for generator of files'''
+        attributes = [i for i in dir(self) if not re.match(r'^\_[a-z]', i)] + [i.stem for i in self._get_all_files()]
+        args = f'{self.path.stem} ({self.__class__.__name__} instance)', attributes
+        return str_ + self._p_string.format(*args)
+    
     @staticmethod
     def add(*args: dict, **kwargs):
         '''
             args: dict of all files loaded from DataLoader to be added with.
-            kwargs: key<int: 1>
+            kwargs: kwarg=key_name
             Ex:
                 DataLoader.add(*args, key1='')
         '''
-        all_data = [i for i in args]
-        all_folder_names = tuple(kwargs.get(f'key{idx}') for idx,_ in enumerate(kwargs.items(), start=1))
-        all_added_files = OrderedDict({folder_name: folder_files for folder_name, folder_files in zip(all_folder_names, all_data)})
+        all_folder_names = [str(i) for i in range(1, len(args)+1)] if not kwargs else tuple(kwargs.values())
+        all_added_files = ArgMapper({folder_name: folder_files for folder_name, folder_files in zip(all_folder_names, args)})
         return all_added_files
 
     def load_data(self, file_path: Path):
-        def _load_and_map():
+        def json_map():
             try:
                 with open(self.path / f'{file_name}.{self.file_ext}', mode='r', encoding='utf-8') as file:
                     data = json.load(file)
@@ -38,23 +104,33 @@ class DataLoader:
             except json.JSONDecodeError as e:
                 raise ValueError(f"Error decoding JSON in {file_name}: {e}")
         
+        def ext_map():
+            '''Mainly for files without any extension (e.g corpora/stopwords/*)'''
+            try: return open(self.path / file_name, mode='r', encoding='utf-8').read().splitlines()
+            except FileNotFoundError: raise FileNotFoundError(f"Data file not found at {file_name}")
+        
         file_name = file_path.stem 
-        data = _load_and_map()
+        data = json_map() if not self.ext_path else ext_map()
         return file_name, data
 
-    def __call__(self):
-        all_files = {}
+    def __call__(self, mapper: bool=False):
+        all_files = OrderedDict()
+        all_file_paths = []
         with ThreadPoolExecutor(max_workers=cpu_count() // 2) as executor:
-            future_to_file = {executor.submit(self.load_data, file_path): file_path for file_path in self.data_files}
-
+            future_to_file = {executor.submit(self.load_data, file_path): file_path for file_path in self._data_files}
             for future in as_completed(future_to_file):
                 file_path = future_to_file[future]
+                all_file_paths.append(file_path)
                 try:
                     file_name, data = future.result()
                     all_files[file_name] = data
-                except Exception:
-                    ...
-        return all_files
+                except Exception: ...
+        contains_dirs = any([i.is_dir() for i in all_file_paths])
+        if mapper and not contains_dirs:
+            return ArgMapper(all_files, _p_key=self.path.stem)
+        elif contains_dirs:
+            return ArgMapper(all_files, _p_key=self.path.stem, dont_map=True, paths=all_file_paths)
+        return self
 
     @staticmethod
     def load_file(path='', file_name='', ext='json', **kwargs):
@@ -73,20 +149,15 @@ class DataLoader:
         if ext=='json':
             file = json.load(file)
         return file
-    
+
     @property
-    def get_files(self):
-        return self.data_files
+    def mapper(self):
+        return self(mapper=True)
 
     @classmethod
     @lru_cache(maxsize=1)
     def _translate_text(cls, *args):
-        import tensorflow as tf
         from transformers import MarianConfig, MarianMTModel, MarianTokenizer, pipeline
-        import gensim
-        from gensim.models import Word2Vec
-        from nltk.tokenize import word_tokenize
-
         text, src_lang, tgt_lang = args
         model_name = f'Helsinki-NLP/opus-mt-{src_lang}-{tgt_lang}'
         config = MarianConfig.from_pretrained(model_name, revision="main")
@@ -100,7 +171,35 @@ class DataLoader:
     def translate(cls, *args):
         return cls._translate_text(*args)
 
+@lru_cache(maxsize=1)
+def loader(key='jsons'):
+    '''
+    This function is mainly for this projects data rather external.
+    Use DataLoader (dl) ext_path argument if needed.
+    (E.g dl(ext_path=f'{Path.home()}/nltk_data/corpora/stopwords)(mapper: bool=False))
+    
+    Returns ArgMapper instance of folder for given key name.
+    
+    E.g
+    all_hadiths = loader(key='hadiths)
+    all_hadiths.<file_name> or use regular dictionary `get` methods including brackets
+    
+    To return all folders at once (Temporary folder name can be changed to personal liking), simply do:
+    {folder: loader(key=folder) for _,folder in enumerate(('jsons', 'hadiths', 'pillars', 'salah'))}
+    '''
+    global stopwords
+    
+    dl = DataLoader
+    hadiths, pillars, salah = (dl(folder_path=f'jsons/{i}').mapper for _,i in enumerate(('hadiths', 'pillars', 'salah')))
+    #^ All NLTK Stopwords
+    stopwords = dl(ext_path=f'{Path.home()}/nltk_data/corpora/stopwords').mapper
+    return dl().mapper if key=='jsons' else locals().get(key)
+
 def main():
-    return DataLoader(folder_path='jsons', file_ext='json')()
+    return DataLoader(folder_path='jsons', file_ext='json').mapper
+
 if __name__ == '__main__':
     main()
+
+#** Initiates NLTK stopwords globally
+loader()
